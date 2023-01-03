2312a54d77b9e08fcf58cfced499726477fe3538
WangYuliang <langzi1206@msn.cn>
2023-01-03 15:11:52 +0800

    增加 Vuex 数据不是持久化存储的说明

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,9 @@ import Vuex from "vuex"
 
 Vue.use(Vuex) // Vuex 是一个插件，需要通过 Vue.use() 方法来使用
 
+// 注意：Vuex 管理的数据是不是持久化的，当刷新页面时 Vuex 会被重置从而导致其管理的数据丢失
+// 若要实现 Vuex 数据的持久化存储，需结合 localStorage 或者 sessionStorage
+
 // 创建 Vuex 的 store
 const store = new Vuex.Store({
   // state 用于存储状态，通过 store.state.xxx 方法读取状态信息
